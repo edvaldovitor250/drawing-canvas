c62bad61abcaeacd76f1771765e8c552dfddb89b
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-03-16 21:56:10 -0300

    add: function for draw

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let currentColor = 'black'
 let canDraw = false
+let mouseX = 0
+let mouseY = 0
 
 let screen = document.querySelector('#tela')
 let ctx = screen.getContext('2d')
@@ -26,15 +28,35 @@ e.target.classList.add('active')
 
 function mouseDownEvent(){
  canDraw = true
+ mouseX = e.pageX - screen.offsetLeft
+ mouseY = e.pageY - screen.offsetTop
 }
 
-function mouseMoveEvent(){
+function mouseMoveEvent(e){
 if (canDraw) {
-    canDraw
-    
+    draw(e.pageX, e.pageY)
 }
 }
 
 function mouseUpEvent(){
 canDraw = false
+}
+
+function draw(x,y){
+    let pointX = x - screen.offsetLeft
+    let pointY = y - screen.offsetTop
+
+    ctx.beginPath();
+    ctx.lineWidth = 5
+    ctx.lineJoin = 'round'
+    ctx.moveTo(mouseX,mouseY)
+    ctx.lineTo(pointX,pointY)
+    ctx.closePath();
+    ctx.strokeStyle = currentColor
+    ctx.stroke()
+
+
+
+    mouseX = pointX
+    mouseY = pointY
 }
