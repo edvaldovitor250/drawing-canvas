96c9913b8e854a7d8425b9ba2e3853cf7d28be3b
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-03-16 21:40:35 -0300

    add: logic for the mouse

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 let currentColor = 'black'
+let canDraw = false
+
+let screen = document.querySelector('#tela')
+let ctx = screen.getContext('2d')
 
 document.querySelectorAll('.colorArea .color').forEach(item =>{
     item.addEventListener('click', colorClickEvent)
 })
 
+screen.addEventListener('mousedown',mouseDownEvent)
+screen.addEventListener('mousemove',mouseMoveEvent)
+screen.addEventListener('mouseup',mouseUpEvent)
+
+
 function colorClickEvent(e){
+
 let color = e.target.getAttribute('data-color')
 currentColor = color
 
 document.querySelector('.color.active').classList.remove('active')
-
 e.target.classList.add('active')
 
+}
+
+
+function mouseDownEvent(){
+ canDraw = true
+}
+
+function mouseMoveEvent(){
+if (canDraw) {
+    canDraw
+    
+}
+}
+
+function mouseUpEvent(){
+canDraw = false
 }
